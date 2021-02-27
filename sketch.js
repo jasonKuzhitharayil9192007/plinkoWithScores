5f5212c437d75a360fcd8fb719d1a85524f4ed33
jasonKuzhitharayil9192007 <72137271+jasonKuzhitharayil9192007@users.noreply.github.com>
2021-02-27 17:52:16 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -63,88 +63,19 @@ function setup() {
 function draw() {
     background(0);
     Engine.update(engine);
-    textSize(35)
-  text("Score : "+score,20,40);
-  fill("red");
   
-  textSize(35)
-  text(" 500 ", 5, 550);
-  text(" 500 ", 80, 550);
-  text(" 500 ", 160, 550);
-  text(" 500 ", 240, 550);
-  text(" 100 ", 320, 550);
-  text(" 100 ", 400, 550);
-  text(" 100 ", 480, 550);
-  text(" 200 ", 560, 550);
-  text(" 200 ", 640, 550);
-  text(" 200 ", 720, 550);
-  
-
-
-
     ground.display();
-    if ( gameState =="END") {
-        background("black");
-        fill("red");
-        textSize(100);
-        text("Game Over", 200, 400);
-       
-      } 
-      for(var k = 0; k < plinkos.length; k++) {
-       plinkos[k].display();
-    }
-
-    if(particle!=null)
-    {
-       particle.display();
-        
-        if (particle.body.position.y>700)
-        {
-              if (particle.body.position.x < 300) 
-              {
-                  score=score+500;      
-                  particle=null;
-                  if ( count>= 5) gameState ="END";                          
-              }
-
-
-              else if (particle.body.position.x < 600 && particle.body.position.x > 301 ) 
-              {
-                    score = score + 100;
-                    particle=null;
-                    if ( count>= 5) gameState ="END";
-
-              }
-              else if (particle.body.position.x < 900 && particle.body.position.x > 601 )
-              {
-                    score = score + 200;
-                    particle=null;
-                    if ( count>= 5)  gameState ="END";
-
-              }      
-              
-        }
-    }
-    
-
    
-    
-  
- for (var i = 0; i < divisions.length; i++) {
-     
-    divisions[i].display();
-  }
+ 
   
 }
 
+function spawnParticles()
+{
 
-function mousePressed() {
-    if(gameState !== "END") {
-        count++;
-    particle = new Particle(mouseX, 50, 10, 10);
-    }
+
+
+
 }
-
-
 
 

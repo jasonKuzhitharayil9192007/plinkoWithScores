79e2dad9fd7c1864abd6eaabe76bfc97579d5940
jasonKuzhitharayil9192007 <72137271+jasonKuzhitharayil9192007@users.noreply.github.com>
2021-02-27 19:06:23 -0500

    Update Division.js

--- Division.js
+++ Division.js
@@ -10,7 +10,8 @@ class Division {
        World.add(world,this.body);
     }
     display(){
-    var pos = this.body.position;
+    push();
+        var pos = this.body.position;
     rectMode(CENTER);
     fill("white");
     rect(pos.x,pos.y,this.width,this.height);

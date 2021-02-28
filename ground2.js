2f202dbed0947428b3364357ba8ce1c2419e1ca9
jasonKuzhitharayil9192007 <72137271+jasonKuzhitharayil9192007@users.noreply.github.com>
2021-02-27 19:07:27 -0500

    Update ground2.js

--- ground2.js
+++ ground2.js
@@ -10,7 +10,8 @@ class Ground2 {
        World.add(world,this.body);
     }
     display(){
-    var pos = this.body.position;
+    push();
+        var pos = this.body.position;
     rectMode(CENTER);
     fill("brown");
     rect(pos.x,pos.y,this.width,this.height);

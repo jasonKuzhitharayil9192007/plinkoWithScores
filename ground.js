0c1c203ece2f70a7705514d52af49f13239f8496
jasonKuzhitharayil9192007 <72137271+jasonKuzhitharayil9192007@users.noreply.github.com>
2021-02-27 19:07:06 -0500

    Update ground.js

--- ground.js
+++ ground.js
@@ -10,7 +10,8 @@ class Ground {
        World.add(world,this.body);
     }
     display(){
-    var pos = this.body.position;
+    push();
+        var pos = this.body.position;
     rectMode(CENTER);
     fill(255);
     rect(pos.x,pos.y,this.width,this.height);
